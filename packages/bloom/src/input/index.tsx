22a273139a1f971a99d031088ecc0b37a6fa43c2
Sébastien VIAL <sebastien@shyrogan.fr>
2026-04-12 20:47:35 +0200

    feat: add InputError component to bloom input

--- packages/bloom/src/input/index.tsx
+++ packages/bloom/src/input/index.tsx
@@ -1,12 +1,15 @@
 import { InputRoot } from "./input-root.tsx";
 import { InputLabel } from "./input-label.tsx";
+import { InputError } from "./input-error.tsx";
 
 type InputType = typeof InputRoot & {
   Root: typeof InputRoot;
   Label: typeof InputLabel;
+  Error: typeof InputError;
 };
 
 export const Input: InputType = Object.assign(InputRoot, {
   Root: InputRoot,
   Label: InputLabel,
+  Error: InputError,
 });
---
+++ packages/bloom/src/input/input-error.tsx
@@ -0,0 +1,17 @@
+import { type ComponentProps, type JSX, splitProps } from "solid-js";
+import { cva } from "../cva.ts";
+
+const errorVariants = cva({
+  base: ["text-destructive text-xs mt-1"],
+});
+
+type InputErrorProps = ComponentProps<"span">;
+
+export function InputError(props: InputErrorProps): JSX.Element {
+  const [local, rest] = splitProps(props, ["class", "children"]);
+  return (
+    <span {...rest} class={errorVariants({ class: local.class })}>
+      {local.children}
+    </span>
+  );
+}
