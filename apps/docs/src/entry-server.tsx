afef188f889bc549e1d2166f678d188f2d3e1803
Sébastien VIAL <sebastien@shyrogan.fr>
2026-04-12 21:49:40 +0200

    fix(docs): apply properly styling

--- apps/docs/src/entry-server.tsx
+++ apps/docs/src/entry-server.tsx
@@ -9,6 +9,12 @@ export default createHandler(() => (
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@500;600;700;800&display=swap"
+          />
           {assets}
         </head>
         <body>
